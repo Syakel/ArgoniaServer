dc01444d800e25a78ed4fe348aecebf3219aa6e3
Syakel <51433117+Syakel@users.noreply.github.com>
2020-08-05 20:45:16 -0500

    Initial commit

---
+++ Client/Client.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Client
+    {
+        public static Client instance;
+        public static int dataBufferSize = 4096;
+
+        public string ip = "127.0.0.1";
+        public int port = 6854;
+        public int myId = 0;
+        public TCP tcp;
+
+        public Client()
+        {
+            tcp = new TCP();
+        }
+
+        public void ConnectToServer()
+        {
+            tcp.Connect();
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+
+            public void Connect()
+            {
+                socket = new TcpClient
+                {
+                    ReceiveBufferSize = dataBufferSize,
+                    SendBufferSize = dataBufferSize
+                };
+
+                receiveBuffer = new byte[dataBufferSize];
+                socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
+            }
+
+            private void ConnectCallback(IAsyncResult _result)
+            {
+                socket.EndConnect(_result);
+
+                if (!socket.Connected)
+                {
+                    return;
+                }
+
+                stream = socket.GetStream();
+
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+
+            private void ReceiveCallback(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLength = stream.EndRead(_result);
+                    if (_byteLength <= 0)
+                    {
+                        return;
+                    }
+
+                    byte[] _data = new byte[_byteLength];
+                    Array.Copy(receiveBuffer, _data, _byteLength);
+
+                    stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+                }
+                catch (Exception _ex)
+                {
+                    Console.WriteLine($"Error receiving TCP data: {_ex}");
+                }
+            }
+        }
+    }
+}
---
+++ Server/ServerHandle.cs
@@ -0,0 +1,30 @@
+﻿using GameServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+            if (_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+            }
+            // TODO: Send player into game
+        }
+
+        public static void UDPTestReceived(int _fromClient, Packet _packet)
+        {
+            string _msg = _packet.ReadString();
+
+            Console.WriteLine($"Received packet via UDP. Message: {_msg}");
+        }
+    }
+}
